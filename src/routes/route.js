94b65fa0d0c80d0738c1538357cca7e922eb793b
Harsh2313 <kaushikh464@gmail.com>
2022-07-06 16:33:55 +0530

    user & Book API

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,35 @@
+// ================ imports ===========================================================================================//
+
 const express = require('express');
+
+const {createUser,loginUser} = require('../controllers/userController');
+
+const {createBook,getBooks,getBookById,updateBookById,deleteBookById} = require('../controllers/bookController');
+
+
+const {authentication} = require('../middleware/middleware');
+
 const router = express.Router();
-const userController = require('../controllers/userController')
 
-router.post('/register', userController.createUser)
+// ================ user apis ===========================================================================================//
+
+router.post('/register',createUser); 
+
+router.post('/login',loginUser); 
+
+// ================ book apis ===========================================================================================//
+
+router.post('/books',authentication,createBook); 
+
+router.get('/books',authentication,getBooks); 
+
+router.get('/books/:bookId',authentication,getBookById); 
+
+router.put('/books/:bookId',authentication,updateBookById); 
+
+router.delete('/books/:bookId',authentication,deleteBookById); 
 
 
+// =========================== exports =======================================================================================//
 
 module.exports = router;
